d451656501d48504f49764b263a5594eb5e900a6
Kyle Lynch <kylelynch104@gmail.com>
2019-04-03 10:54:03 -0800

    Updates

---
+++ precache-manifest.ce7435b9587667671a72bc8cd23ca0b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "894bb4ed88afee36b04d",
+    "url": "/React_Todo/static/js/runtime~main.6c87acfc.js"
+  },
+  {
+    "revision": "854b39d66330a801e8a3",
+    "url": "/React_Todo/static/js/main.189fc463.chunk.js"
+  },
+  {
+    "revision": "254a14c09bfef11176c1",
+    "url": "/React_Todo/static/js/2.ccc89068.chunk.js"
+  },
+  {
+    "revision": "854b39d66330a801e8a3",
+    "url": "/React_Todo/static/css/main.02b1e9c7.chunk.css"
+  },
+  {
+    "revision": "7adad03a46524b35a6b0ed74a22a6fbb",
+    "url": "/React_Todo/index.html"
+  }
+];
